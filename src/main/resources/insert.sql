6dbaf720d073840cf62df71ab568bf2a9de6abc5
maksimionov <ionovmk@gmail.com>
2023-09-03 20:50:22 +0200

    add populational sql script for hibernate

---
+++ src/main/resources/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO product (calories, carb, fat, productName, protein, productId) VALUES (350.0, 70.0, 12.0, 'oat', 12.0, 1);
+INSERT INTO product (calories, carb, fat, productName, protein, productId) VALUES (350.0, 70.0, 12.0, 'egg', 12.0, 2);
+INSERT INTO product (calories, carb, fat, productName, protein, productId) VALUES (350.0, 70.0, 12.0, 'banana', 12.0, 3);
+INSERT INTO product (calories, carb, fat, productName, protein, productId) VALUES (350.0, 70.0, 12.0, 'salmon', 12.0, 4);
+INSERT INTO product (calories, carb, fat, productName, protein, productId) VALUES (350.0, 70.0, 12.0, 'rise', 12.0, 5);
